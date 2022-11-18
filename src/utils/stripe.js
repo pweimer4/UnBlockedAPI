3916db4a54de20487822c52d2068f6f34e9dfc03
p.weimer <78502799+pweimer4@users.noreply.github.com>
2022-11-18 09:55:26 +0100

    Delete src directory

--- src/utils/stripe.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stripe = void 0;
-const stripe_1 = require("stripe");
-exports.stripe = new stripe_1.default(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2022-08-01"
-});
